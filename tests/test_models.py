ac9408dff7ef4cd5a8727d4712b2f8c0798d5420
formakidov <formakidov1996@gmail.com>
2025-07-18 18:22:32 +0200

    Implements missing 'find by name' test for models (exercise 2)

--- tests/test_models.py
+++ tests/test_models.py
@@ -180,3 +180,15 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+     def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
